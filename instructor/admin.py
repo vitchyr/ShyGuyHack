232da0a1347e26e079bf73d8f943755b020301ba
vpong <vitchyr@gmail.com>
2014-02-08 19:12:36 -0500

    Test website

---
+++ instructor/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from instructor.models import Course, Tag
+
+# Register your models here.
+admin.site.register(Course)
+admin.site.register(Tag)
--- shyguy/urls.py
+++ shyguy/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'shyguy.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^instructor/', include('instructor.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
