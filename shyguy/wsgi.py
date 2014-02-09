0cf3773c4888d6de3b0e32ea9a7b1743376a370c
vpong <vitchyr@gmail.com>
2014-02-09 02:03:46 -0500

    changed wsgi according to https://devcenter.heroku.com/articles/django-assets

--- shyguy/wsgi.py
+++ shyguy/wsgi.py
@@ -10,4 +10,7 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shyguy.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+#application = get_wsgi_application()
+
+from dj_static import Cling
+application = Cling(get_wsgi_application())
