806a51081261540ce50b4cb90a143b3a4570f7c0
vpong <vitchyr@gmail.com>
2014-02-08 21:11:40 -0500

    Ready to integrate javascript that makes ajax call get_data

--- courses/urls.py
+++ courses/urls.py
@@ -5,5 +5,6 @@ from courses import views
 urlpatterns = patterns('',
   url(r'^$', views.index, name='index'),
   url(r'^create/$', views.create, name='create'),
+  url(r'^get_data/$', views.get_data, name='get_data'),
   url(r'^(?P<course_id>\d+)/$', views.details, name='details'),
 )
--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+import json
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from courses.models import Course, Tag
@@ -12,14 +12,29 @@ def index(request):
 def create(request):
   c = Course(name='test course',description='test descript')
   c.save()
-  return HttpResponse('Course created')
+  return HttpResponse('Course created with id = ' + str(c.pk))
 
 def details(request, course_id):
-  c = get_object_or_404(Course, pk=course_id)
-  #netid = request.POST['netid']
-  netid="vhp22"
-  pop = 5
-  #tags = request.POST['tags']
-  t = Tag(course=c, name=netid, count=pop)
-  t.save()
-  return HttpResponse(str(t))
+  if request.method == 'POST':  
+    c = get_object_or_404(Course, pk=course_id)
+    #netid = request.POST['netid']
+    netid="vhp22"
+    pop = 5
+    #tags = request.POST['tags']
+    t = Tag(course=c, name=netid, count=pop)
+    t.save()
+    return HttpResponse(str(t))
+  else:
+    context = {}
+    return render(request, 'courses/instructor_view.html', context)
+
+def get_data(request):
+  #course_id = request.POST['course_id']
+  course_id = 1
+  course_tags = Tag.objects.filter(course=course_id)
+  tag_count = 0
+  for t in course_tags:
+    tag_count += 1
+  data = {'count': tag_count}
+  output = json.dumps(data)
+  return output
