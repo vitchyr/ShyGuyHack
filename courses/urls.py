676e867eda50daefaacc71bdd3a74a9c620fe492
vpong <vitchyr@gmail.com>
2014-02-08 21:31:29 -0500

    Made voting a get request

--- courses/urls.py
+++ courses/urls.py
@@ -5,6 +5,7 @@ from courses import views
 urlpatterns = patterns('',
   url(r'^$', views.index, name='index'),
   url(r'^create/$', views.create, name='create'),
-  url(r'^get_data/$', views.get_data, name='get_data'),
+  url(r'^(?P<course_id>\d+)/get_data$', views.get_data, name='get_data'),
+  url(r'^vote/(?P<course_id>\d+)/$', views.vote, name='vote'),
   url(r'^(?P<course_id>\d+)/$', views.details, name='details'),
 )
--- courses/views.py
+++ courses/views.py
@@ -14,27 +14,28 @@ def create(request):
   c.save()
   return HttpResponse('Course created with id = ' + str(c.pk))
 
-def details(request, course_id):
-  if request.method == 'POST':  
-    c = get_object_or_404(Course, pk=course_id)
-    #netid = request.POST['netid']
-    netid="vhp22"
-    pop = 5
-    #tags = request.POST['tags']
-    t = Tag(course=c, name=netid, count=pop)
-    t.save()
-    return HttpResponse(str(t))
-  else:
-    context = {}
-    return render(request, 'courses/instructor_view.html', context)
+def vote(request, course_id):
+  c = get_object_or_404(Course, pk=course_id)
+  #netid = request.POST['netid']
+  netid="vhp22"
+  pop = 5
+  #tags = request.POST['tags']
+  t = Tag(course=c, name=netid, count=pop)
+  t.save()
+  return HttpResponse(str(t))
 
-def get_data(request):
+def details(request, course_id):
+  context = {}
+  return render(request, 'courses/instructor_view.html', context)
+
+def get_data(request, course_id):
   #course_id = request.POST['course_id']
-  course_id = 1
+  #course_id = 1
+  print("get_data called")
   course_tags = Tag.objects.filter(course=course_id)
   tag_count = 0
   for t in course_tags:
     tag_count += 1
   data = {'count': tag_count}
   output = json.dumps(data)
-  return output
+  return HttpResponse(output)
