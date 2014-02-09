99fa9dad9afeec797cd4f93fb18e464b5ee1d104
vpong <vitchyr@gmail.com>
2014-02-09 00:38:53 -0500

    fixing static files in heroku

--- shyguy/settings.py
+++ shyguy/settings.py
@@ -81,4 +81,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+#STATIC_URL = '/static/'
+
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_PATH, 'static'),
+)
